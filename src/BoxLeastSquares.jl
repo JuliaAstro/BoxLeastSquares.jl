e447d1f308e689ac86de955ad3b712c80127ed0e
Miles Lucas <mdlucas@hawaii.edu>
2021-05-19 02:10:41 -0500

    get naive version working

--- src/BoxLeastSquares.jl
+++ src/BoxLeastSquares.jl
@@ -1,5 +1,9 @@
 module BoxLeastSquares
 
+using Statistics
+
+export bls, autoperiod, power, period
+
 include("core.jl")
 
 end
--- src/core.jl
+++ src/core.jl
@@ -4,17 +4,33 @@ struct BLSPeriodogram{TT,VT<:AbstractVector{TT},YT<:AbstractVector,ET<:AbstractV
     t::VT
     y::YT
     err::ET
+    period
+    duration
+    power
+    method::Symbol
+end
+
+period(bls::BLSPeriodogram) = bls.period
+duration(bls::BLSPeriodogram) = bls.duration
+power(bls::BLSPeriodogram) = bls.power
+
+function Base.show(io::IO, ::MIME"text/plain", bls::BLSPeriodogram)
+    best_ind = argmax(power(bls))
+    best_pow = power(bls)[best_ind]
+    best_per = period(bls)[best_ind]
+    method = string(bls.method)
+    print(io, "BLSPeriodogram\n", "best period: ", best_per, "\n$method at best period: ", best_pow)
 end
 
 
-function autoperiod(bls::BLSPeriodogram,
+function autoperiod(t,
                     duration;
-                    minimum_period=default_minimum_period(duration),
-                    maximum_period=default_maximum_period(bls, minimum_n_transit),
                     minimum_n_transit=3,
+                    minimum_period=default_minimum_period(duration),
+                    maximum_period=default_maximum_period(t, minimum_n_transit),
                     frequency_factor=1.0)
 
-    lo, hi = extrema(bls.t)
+    lo, hi = extrema(t)
     baseline = hi - lo
     df = frequency_factor * minimum(duration) / baseline^2
 
@@ -27,11 +43,69 @@ function autoperiod(bls::BLSPeriodogram,
     maximum_frequency = inv(minimum_period)
 
     nf = (maximum_frequency - minimum_frequency) ÷ df
-    return @. maximum_frequency - df * (0:nf)
+    return @. inv(maximum_frequency - df * (0:nf))
 end
 
 default_minimum_period(duration) = 2 * maximum(duration)
-function default_maximum_period(bls, minimum_n_transit)
-    lo, hi = extrema(bls.t)
+function default_maximum_period(t, minimum_n_transit)
+    minimum_n_transit > 1 || error("minimum number of transits must be greater than 1")
+    lo, hi = extrema(t)
     return (hi - lo) / (minimum_n_transit - 1)
+end
+
+function bls(t, y, yerr;
+               duration,
+               objective=:likelihood,
+               oversample=10,
+               minimum_n_transit=3,
+               minimum_period=default_minimum_period(duration),
+               maximum_period=default_maximum_period(t, minimum_n_transit),
+               frequency_factor=1.0,
+               period=autoperiod(t,
+                                 duration;
+                                 minimum_n_transit=minimum_n_transit,
+                                 minimum_period=minimum_period,
+                                 maximum_period=maximum_period,
+                                 frequency_factor=frequency_factor))
+    invvar = @. inv(yerr^2)
+    y_res = y .- median(y)
+
+    power = map(period) do P
+        best = -Inf
+        half_P = 0.5 * P
+        min_t = minimum(t)
+        for τ in duration
+            # compute phase grid
+            dp = τ / oversample
+            phase = 0:dp:P
+
+            for t0 in phase
+                # find in-transit data points
+                mask = @. abs((t - min_t - t0 + half_P) % P - half_P) < 0.5 * τ
+                imask = .!mask
+
+                # estimate in-transit flux
+                invvar_in = sum(invvar[mask])
+                y_in = mapreduce(*, +, y[mask], invvar[mask]) / invvar_in
+                # estimate out-of-transit flux
+                invvar_out = sum(invvar[imask])
+                y_out = mapreduce(*, +, y[imask], invvar[imask]) / invvar_out
+
+                # compute best-fit depth
+                if objective === :snr
+                    depth = y_out - y_in
+                    depth_err = sqrt(inv(invvar_in) + inv(invvar_out))
+                    snr = depth / depth_err
+                    best = max(best, snr)
+                elseif objective === :likelihood
+                    loglike = -0.5 * mapreduce((y,iv) -> ((y - y_in)^2 - (y - y_out)^2) * iv, +, y[mask], invvar[mask])
+                    best = max(best, loglike)
+                else
+                    error("invalid objective $objective. Should be one of `:snr` or `:likelihood`")
+                end
+            end
+        end
+        return best
+    end
+    return BLSPeriodogram(t, y, yerr, period, duration, power, objective)
 end
