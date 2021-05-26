35669bedb5910b0f36dfb8dcac17f2e8af3cf84f
Miles Lucas <mdlucas@hawaii.edu>
2021-05-26 15:33:05 -0500

    clean up some code

--- src/core.jl
+++ src/core.jl
@@ -36,105 +36,47 @@ function Base.show(io::IO, ::MIME"text/plain", bls::BLSPeriodogram)
     method = string(bls.method)
     n = length(bls.t)
     m = length(bls.power)
-    print(io, "BLSPeriodogram\n==============\ninput dim: ", n, "\noutput dim: ", m, "\nperiod range: ", range_str(extrema(bls.period)), "\nduration range: ", range_str(extrema(bls.duration)), "\n\nparameters\n----------\n", "period: ", p.period, "\nduration: ", p.duration, "\nt0: ", p.t0, "\n$method: ", p.power)
+    println(io, "BLSPeriodogram\n==============")
+    println(io, "input dim: ", n)
+    println(io, "output dim: ", m)
+    println(io, "period range: ", range_str(extrema(bls.period)))
+    println(io, "duration range: ", range_str(extrema(bls.duration_in)))
+    println(io, "\nparameters\n----------")
+    println(io, "period: ", p.period)
+    println(io, "duration: ", p.duration)
+    println(io, "t0: ", p.t0)
+    print(io, "$method: ", p.power)
 end
 
 range_str((min, max)) = "$min - $max"
 
 
-function autoperiod(t,
+function autoperiod(t::AbstractVector{T},
                     duration;
                     minimum_n_transit=3,
                     minimum_period=default_minimum_period(duration),
                     maximum_period=default_maximum_period(t, minimum_n_transit),
-                    frequency_factor=1.0)
-
-    lo, hi = extrema(t)
-    baseline = hi - lo
+                    frequency_factor=1.0) where T
+    baseline = maximum(t) - minimum(t)
     df = frequency_factor * minimum(duration) / baseline^2
 
     if maximum_period < minimum_period
         minimum_period, maximum_period = maximum_period, minimum_period
     end
-    minimum_period ≥ zero(minimum_period) || error("minimum period must be positive")
+    minimum_period ≥ zero(T) || error("minimum period must be positive")
 
     minimum_frequency = inv(maximum_period)
     maximum_frequency = inv(minimum_period)
 
     nf = 1 + (maximum_frequency - minimum_frequency) ÷ df
-    return @. inv(maximum_frequency - df * (0:nf))
+    return @. convert(T, inv(maximum_frequency - df * (0:nf)))
 end
 
 default_minimum_period(duration) = 2 * maximum(duration)
 function default_maximum_period(t, minimum_n_transit)
     minimum_n_transit > 1 || error("minimum number of transits must be greater than 1")
-    lo, hi = extrema(t)
-    return (hi - lo) / (minimum_n_transit - 1)
-end
-
-function BLS_slow(t, y, yerr;
-               duration,
-               objective=:likelihood,
-               oversample=10,
-               period=autoperiod(t, duration))
-    invvar = @. inv(yerr^2)
-    y_res = y .- median(y)
-    min_t = minimum(t)
-
-    # set up arrays
-    powers = similar(period, Float64)
-    durations = similar(period, typeof(duration))
-    t0s = similar(period)
-
-    @inbounds Threads.@threads for idx in eachindex(period)
-        P = period[idx]
-        best = (power=-Inf, duration=zero(P), t0=zero(P))
-        half_P = 0.5 * P
-        for τ in duration
-            # compute phase grid
-            dp = τ / oversample
-            phase = zero(P):dp:P
-
-            for t0 in phase
-                # find in-transit data points
-                invvar_in = invvar_out = zero(eltype(invvar))
-                y_in = y_out = zero(eltype(y_res))
-                for i in eachindex(t)
-                    transiting = abs((t[i] - min_t - t0 + half_P) % P - half_P) < 0.5 * τ
-                    if transiting
-                        invvar_in += invvar[i]
-                        y_in += y_res[i] * invvar[i]
-                    else
-                        invvar_out += invvar[i]
-                        y_out += y_res[i] * invvar[i]
-                    end
-                end
-                y_in /= invvar_in
-                y_out /= invvar_out
-
-                # compute best-fit depth
-                if objective === :snr
-                    depth = y_out - y_in
-                    depth_err = sqrt(inv(invvar_in) + inv(invvar_out))
-                    snr = depth / depth_err
-                    if snr > best.power
-                        best = (power=snr, duration=τ, t0=t0)
-                    end
-                elseif objective === :likelihood
-                    loglike = 0.5 * invvar_in * (y_out - y_in)^2
-                    if loglike > best.power
-                        best = (power=loglike, duration=τ, t0=t0)
-                    end
-                else
-                    error("invalid objective $objective. Should be one of `:snr` or `:likelihood`")
-                end
-            end
-        end
-        powers[idx] = best.power
-        durations[idx] = best.duration
-        t0s[idx] = best.t0 + min_t
-    end
-    return BLSPeriodogram(t, y, yerr, period, powers, durations, t0s, objective)
+    baseline = maximum(t) - minimum(t)
+    return baseline / (minimum_n_transit - 1)
 end
 
 @inline wrap(x, period) = x - period * floor(x / period)
@@ -144,12 +86,10 @@ function BLS(t, y, yerr;
     objective=:likelihood,
     oversample=10,
     period=autoperiod(t, duration))
-    ymed = median(y)
-    min_t = minimum(t)
 
     # set up arrays
     powers = similar(period, Float64)
-    durations = similar(period, typeof(duration))
+    durations = similar(period, eltype(duration))
     t0s = similar(period)
 
     # find parameter ranges
@@ -162,15 +102,21 @@ function BLS(t, y, yerr;
     mean_ivar = similar(yerr, typeof(inv(first(yerr)^2)), max_bins + 1)
 
     # pre-accumulate some factors
+    ymed = median(y)
     min_t = minimum(t)
-    sum_y = mapreduce((yi, dyi) -> (yi - ymed) / dyi^2, +, y, yerr)
-    sum_ivar = sum(dyi -> inv(dyi)^2, yerr)
+
+    sum_y = zero(eltype(mean_y))
+    sum_ivar = zero(eltype(mean_ivar))
+    for i in eachindex(y)
+        iv = inv(yerr[i]^2)
+        sum_y += (y[i] - ymed) * iv
+        sum_ivar += iv
+    end
 
     # loop over periods in grid search
     @inbounds for idx in eachindex(period)
         P = period[idx]
         n_bins = ceil(Int, P / bin_duration) + oversample
-
         for n in 0:n_bins
             mean_y[begin + n] = zero(eltype(mean_y))
             mean_ivar[begin + n] = zero(eltype(mean_ivar))
@@ -178,8 +124,9 @@ function BLS(t, y, yerr;
 
         for n in 0:length(t) - 1
             ind = round(Int, wrap(t[begin + n] - min_t, P) / bin_duration) + 1
-            mean_y[begin + ind] += (y[begin + n] - ymed) / yerr[begin + n]^2
-            mean_ivar[begin + ind] += inv(yerr[begin + n]^2)
+            iv = inv(yerr[begin + n]^2)
+            mean_y[begin + ind] += (y[begin + n] - ymed) * iv
+            mean_ivar[begin + ind] += iv
         end
 
         # simplify calcualations by wrapping binned values
@@ -229,6 +176,72 @@ function BLS(t, y, yerr;
                     dur = τ * bin_duration
                     t0 = mod(n * bin_duration + 0.5 * dur, P)
                     best = (power=power, duration=dur, t0=t0)
+                end
+            end
+        end
+        powers[idx] = best.power
+        durations[idx] = best.duration
+        t0s[idx] = best.t0 + min_t
+    end
+    return BLSPeriodogram(t, y, yerr, period, duration, powers, durations, t0s, objective)
+end
+
+
+function BLS_slow(t, y, yerr;
+    duration,
+    objective=:likelihood,
+    oversample=10,
+    period=autoperiod(t, duration))
+    invvar = @. inv(yerr^2)
+    y_res = y .- median(y)
+    min_t = minimum(t)
+
+    # set up arrays
+    powers = similar(period, Float64)
+    durations = similar(period, eltype(duration))
+    t0s = similar(period)
+
+    @inbounds for idx in eachindex(period)
+        P = period[idx]
+        best = (power=-Inf, duration=zero(P), t0=zero(P))
+        half_P = 0.5 * P
+        for τ in duration
+            # compute phase grid
+            dp = τ / oversample
+            phase = zero(P):dp:P
+
+            for t0 in phase
+                # find in-transit data points
+                invvar_in = invvar_out = zero(eltype(invvar))
+                y_in = y_out = zero(eltype(y_res))
+                for i in eachindex(t)
+                    transiting = abs((t[i] - min_t - t0 + half_P) % P - half_P) < 0.5 * τ
+                    if transiting
+                        invvar_in += invvar[i]
+                        y_in += y_res[i] * invvar[i]
+                    else
+                        invvar_out += invvar[i]
+                        y_out += y_res[i] * invvar[i]
+                    end
+                end
+                y_in /= invvar_in
+                y_out /= invvar_out
+
+                # compute best-fit depth
+                if objective === :snr
+                    depth = y_out - y_in
+                    depth_err = sqrt(inv(invvar_in) + inv(invvar_out))
+                    snr = depth / depth_err
+                    if snr > best.power
+                        best = (power=snr, duration=τ, t0=t0)
+                    end
+                elseif objective === :likelihood
+                    loglike = 0.5 * invvar_in * (y_out - y_in)^2
+                    if loglike > best.power
+                        best = (power=loglike, duration=τ, t0=t0)
+                    end
+                else
+                    error("invalid objective $objective. Should be one of `:snr` or `:likelihood`")
                 end
             end
         end
