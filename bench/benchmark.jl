b16c37a7957d24f114e2dc4f3a7280abd1cfa8cd
Miles Lucas <mdlucas@hawaii.edu>
2021-08-16 01:21:38 -1000

    flesh out benchmarks

--- bench/benchmark.jl
+++ bench/benchmark.jl
@@ -1,8 +1,12 @@
 using BenchmarkTools
 using BoxLeastSquares
-using BoxLeastSquares: period, power
 using PyCall
+using ProgressLogging
 using Random
+using Statistics
+
+ENV["OMP_NUM_THREADS"] = 1
+@info "OMP NUM THREADS=$(ENV["OMP_NUM_THREADS"])"
 
 apt = pyimport("astropy.timeseries")
 
@@ -18,21 +22,36 @@ end
 
 t, y, yerr = gendata(1000)
 
-function periodogram_astropy(t, y, yerr)
+periods = autoperiod(t, 0.2)
+
+function periodogram_astropy(t, y, yerr, periods)
     model = apt.BoxLeastSquares(t, y, dy=yerr)
-    periodogram = model.autopower(0.2)
+    periodogram = model.power(periods, 0.2)
     return model, periodogram
 end
 
-function periodogram_boxleastsquares(t, y, yerr)
-    periodogram = BLS(t, y, yerr; duration=0.2)
-    return periodogram
+function periodogram_boxleastsquares(t, y, yerr, periods)
+    return BLS(t, y, yerr; duration=0.2, periods)
 end
 
-bench_py = @benchmark periodogram_astropy($t, $y, $yerr)
-bench_jl = @benchmark periodogram_boxleastsquares($t, $y, $yerr)
+data_sizes = round.(Int, 10 .^ range(2, 4, length=11))
+period_sizes = round.(Int, 10 .^ range(2, 4, length=3))
 
-println("astropy.timeseries")
-print(bench_py)
-println("\nBoxLeastSquares.jl")
-println(bench_jl)
+results = []
+@progress for N_data in data_sizes
+    t, y, yerr = gendata(N_data)
+    true_period = 3
+    @progress for N_per in period_sizes
+        log_periods = range(log(true_period) - 1, log(true_period) + 1, length=N_per)
+        periods = exp.(log_periods)
+
+        bench_jl = @benchmark periodogram_boxleastsquares($t, $y, $yerr, $periods)
+        bench_py = @benchmark periodogram_astropy($t, $y, $yerr, $periods)
+        
+        res = (N_data, N_per, t_astropy=median(bench_py), t_bls=median(bench_jl))
+        @info "" res...
+        push!(results, res)
+    end
+end
+
+CSV.write(joinpath(@__DIR__, "benchmark_results.csv"), results)
