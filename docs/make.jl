1652bfe20c17012ed9b5a749a9f16c7f29855fc1
Ian Weaver <weaveric@gmail.com>
2025-06-06 08:20:17 -0700

    style

--- docs/make.jl
+++ docs/make.jl
@@ -4,16 +4,16 @@ using Documenter
 DocMeta.setdocmeta!(BoxLeastSquares, :DocTestSetup, :(using BoxLeastSquares); recursive=true)
 include("pages.jl")
 makedocs(;
-    modules=[BoxLeastSquares],
-    authors="Miles Lucas <mdlucas@hawaii.edu> and contributors",
-    repo="https://github.com/JuliaAstro/BoxLeastSquares.jl/blob/{commit}{path}#{line}",
-    sitename="BoxLeastSquares.jl",
+    modules = [BoxLeastSquares],
+    authors = "Miles Lucas <mdlucas@hawaii.edu> and contributors",
+    repo = "https://github.com/JuliaAstro/BoxLeastSquares.jl/blob/{commit}{path}#{line}",
+    sitename = "BoxLeastSquares.jl",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
         assets = String[],
         canonical = "https://JuliaAstro.org/BoxLeastSquares/stable/",
     ),
-    pages=pages
+    pages = pages
 )
 
 deploydocs(;
